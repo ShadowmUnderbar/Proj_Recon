9e90ecb860fcf28138bfb204c9c701efcf7c34d5
ShadowmUnderbar <shadowm.underbar@gmail.com>
2023-07-30 23:45:32 +0900

    first_comit

---
+++ Assets/App/Script/Battle/BattleLifetimeScope.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+public class BattleLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+    }
+}
