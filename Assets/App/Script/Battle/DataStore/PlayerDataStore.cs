b89de81efea0565ee778e042ece91680a78f1c14
ShadowmUnderbar <shadowm.underbar@gmail.com>
2024-10-06 20:10:23 +0900

    各ショットのクールダウン処理を追加

--- Assets/App/Script/Battle/DataStore/PlayerDataStore.cs
+++ Assets/App/Script/Battle/DataStore/PlayerDataStore.cs
@@ -1,14 +1,80 @@
 using R3;
+using UnityEngine;
+using VContainer.Unity;
 
 namespace App.Battle.DataStore
 {
-    public class PlayerDataStore : IPlayerDataStore
+    public class PlayerDataStore : IPlayerDataStore, ITickable
     {
         public ReactiveProperty<float> Health { get; } = new();
         public ReactiveProperty<float> MaxHealth { get; } = new();
-        public ReactiveProperty<float> Bullet { get; } = new();
-        public ReactiveProperty<float> MaxBullet { get; } = new();
         public ReactiveProperty<int> IsFocusLeft { get; } = new();
         public ReactiveProperty<int> IsFocusRight { get; } = new();
+
+        public bool CanLeftNormalShot => _leftNomalShotCoolDown <= 0;
+        public bool CanRightNormalShot => _rightNomalShotCoolDown <= 0;
+        public bool CanMergeShot => _mergeShotCoolDown <= 0;
+        public bool CanLeftWaltzShot => _leftWaltzShotCoolDown <= 0;
+        public bool CanRightWaltzShot => _rightWaltzShotCoolDown <= 0;
+
+        private float _leftNomalShotCoolDown;
+        private float _rightNomalShotCoolDown;
+        private float _mergeShotCoolDown;
+        private float _leftWaltzShotCoolDown;
+        private float _rightWaltzShotCoolDown;
+
+
+        public void Tick()
+        {
+            if (_leftNomalShotCoolDown > 0)
+            {
+                _leftNomalShotCoolDown -= Time.deltaTime;
+            }
+
+            if (_rightNomalShotCoolDown > 0)
+            {
+                _rightNomalShotCoolDown -= Time.deltaTime;
+            }
+
+            if(_mergeShotCoolDown > 0)
+            {
+                _mergeShotCoolDown -= Time.deltaTime;
+            }
+
+            if (_leftWaltzShotCoolDown > 0)
+            {
+                _leftWaltzShotCoolDown -= Time.deltaTime;
+            }
+
+            if (_rightWaltzShotCoolDown > 0)
+            {
+                _rightWaltzShotCoolDown -= Time.deltaTime;
+            }
+        }
+
+        public void SetLeftNormalShotCoolDown(float time)
+        {
+            _leftNomalShotCoolDown = time;
+        }
+
+        public void SetRightNormalShotCoolDown(float time)
+        {
+            _rightNomalShotCoolDown = time;
+        }
+
+        public void SetMergeShotCoolDown(float time)
+        {
+            _mergeShotCoolDown = time;
+        }
+
+        public void SetLeftWaltzShotCoolDown(float time)
+        {
+            _leftWaltzShotCoolDown = time;
+        }
+
+        public void SetRightWaltzShotCoolDown(float time)
+        {
+            _rightWaltzShotCoolDown = time;
+        }
     }
 }
--- Assets/App/Script/Battle/Interface/DataStore/IPlayerDataStore.cs
+++ Assets/App/Script/Battle/Interface/DataStore/IPlayerDataStore.cs
@@ -6,9 +6,21 @@ namespace App.Battle.DataStore
     {
         ReactiveProperty<float> Health { get; }
         ReactiveProperty<float> MaxHealth { get; }
-        ReactiveProperty<float> Bullet { get; }
-        ReactiveProperty<float> MaxBullet { get; }
+
+        bool CanLeftNormalShot { get; }
+        bool CanRightNormalShot { get; }
+        bool CanMergeShot { get; }
+        bool CanLeftWaltzShot { get; }
+        bool CanRightWaltzShot{ get; }
+
+        //フォーカス(照準が敵に重なっている)か
         ReactiveProperty<int> IsFocusLeft { get; }
         ReactiveProperty<int> IsFocusRight { get; }
+
+        void SetLeftNormalShotCoolDown(float time);
+        void SetRightNormalShotCoolDown(float time);
+        void SetMergeShotCoolDown(float time);
+        void SetLeftWaltzShotCoolDown(float time);
+        void SetRightWaltzShotCoolDown(float time);
     }
 }
