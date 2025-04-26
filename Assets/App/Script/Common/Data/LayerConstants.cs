be49f6409663d4e20418436a3e7a9bf90c7698ed
ShadowmUnderbar <shadowm.underbar@gmail.com>
2025-04-26 20:14:12 +0900

    ビルド可能であることの確認

--- Assets/App/Script/Common/Data/LayerConstants.cs
+++ Assets/App/Script/Common/Data/LayerConstants.cs
@@ -1,16 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-
-
 namespace App.Common.Data
 {
-
     public static class LayerConstants
     {
         public static int Default = 1 << 0;
         public static int Hitbox = 1 << 6;
-
     }
 }
