4e66392e26b435ad611d83eeba539d5cb7c3aa9e
ShadowmUnderbar <shadowm.underbar@gmail.com>
2026-02-22 16:30:48 +0900

    弾無効処理を変更

--- Assets/App/Script/Battle/Views/PlayerBulletView.cs
+++ Assets/App/Script/Battle/Views/PlayerBulletView.cs
@@ -5,16 +5,19 @@ using R3;
 using R3.Triggers;
 using App.Common.Data;
 using App.Framework;
+using Cysharp.Threading.Tasks;
 
 namespace App.Battle.Views
 {
     public class PlayerBulletView : MonoBehaviour, IBulletView
     {
+        [SerializeField] private TrailRenderer _trailRenderer;
         [SerializeField] private Layer _shooterLayer;
         [SerializeField] private Collider _hitCollider;
 
         private readonly List<int> _hitTargetIds = new();
 
+        private bool _canHit = true;
         private BulletData _bulletData;
         private int _hitCount = 0;
         private int _focusTargetId = 0;
@@ -34,61 +37,84 @@ namespace App.Battle.Views
         private void Awake()
         {
             _hitCollider.OnTriggerEnterAsObservable()
-                .Subscribe(x =>
-                {
-                    if (x.gameObject.layer == _shooterLayer)
-                    {
-                        return;
-                    }
-
-                    if (x.gameObject.CompareTag(TagConstants.Bullet))
-                    {
-                        return;
-                    }
-
-                    if (!x.TryGetComponent<IHitBoxView>(out var hitBox))
-                    {
-                        Destroy(gameObject);
-                        return;
-                    }
-
-                    if (hitBox.Id == _attackerId)
-                    {
-                        return;
-                    }
-
-                    if (_hitTargetIds.Contains(hitBox.Id))
-                    {
-                        return;
-                    }
-
-                    _hitTargetIds.Add(hitBox.Id);
-
-                    hitBox.OnHit(_bulletData.Damage, _attackerId, transform.position, out var canPenetrable);
-
-                    if (_focusTargetId == hitBox.Id)
-                    {
-                        Destroy(gameObject);
-                    }
-
-                    if (!canPenetrable)
-                    {
-                        Destroy(gameObject);
-                    }
-
-                    _hitCount++;
-
-                    if (_focusTargetId != default && _hitCount >= _bulletData.Penetration)
-                    {
-                        Destroy(gameObject);
-                    }
-                })
+                .Where(_ => _canHit)
+                .Subscribe(HitProcess)
                 .AddTo(this);
         }
 
         private void Update()
         {
+            if (!_canHit)
+            {
+                return;
+            }
+
             transform.position += transform.forward * (_bulletData.Speed * Time.deltaTime);
+        }
+
+        private void HitProcess(Collider col)
+        {
+            if (col.gameObject.layer == _shooterLayer)
+            {
+                return;
+            }
+
+            if (col.gameObject.CompareTag(TagConstants.Bullet))
+            {
+                return;
+            }
+
+            if (!col.TryGetComponent<IHitBoxView>(out var hitBox))
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            if (hitBox.Id == _attackerId)
+            {
+                return;
+            }
+
+            if (_hitTargetIds.Contains(hitBox.Id))
+            {
+                return;
+            }
+
+            _hitTargetIds.Add(hitBox.Id);
+
+            hitBox.OnHit(_bulletData.Damage, _attackerId, transform.position, out var canPenetrable);
+
+            if (_focusTargetId == hitBox.Id)
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            if (!canPenetrable)
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            _hitCount++;
+
+            if (_focusTargetId != default && _hitCount >= _bulletData.Penetration)
+            {
+                HitAfterProcess().Forget();
+            }
+        }
+
+        private async UniTask HitAfterProcess()
+        {
+            _canHit = false;
+
+            if (_trailRenderer == null)
+            {
+                return;
+            }
+
+            await UniTask.WaitForSeconds(_trailRenderer.time);
+            Destroy(gameObject);
         }
     }
 }
