1cbf4f2f0ead09d2388a5a5b4dd218b77807f0c5
ShadowmUnderbar <shadowm.underbar@gmail.com>
2026-02-22 17:59:06 +0900

    弾丸処理を共通化

--- Assets/App/Script/Battle/Views/Enemy/Bullet/BaseBulletView.cs
+++ Assets/App/Script/Battle/Views/Enemy/Bullet/BaseBulletView.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using App.Battle.Interface;
 using App.Common.Data;
 using App.Framework;
+using Cysharp.Threading.Tasks;
 using R3;
 using R3.Triggers;
 using UnityEngine;
@@ -9,53 +11,34 @@ namespace App.Battle.Views.Enemy.Bullet
 {
     public class BaseBulletView : MonoBehaviour, IBulletView
     {
-        [SerializeField] private Collider _hitCollider;
+        [SerializeField] private TrailRenderer _trailRenderer;
         [SerializeField] private Layer _shooterLayer;
+        [SerializeField] private Collider _hitCollider;
 
-        protected float Speed;
+        private readonly List<int> _hitTargetIds = new();
+
+        protected bool CanHit { get; private set; } = true;
+        protected BulletData BulletData { get; private set; }
+        private int _hitCount = 0;
+        private int _focusTargetId = 0;
+        private int _attackerId = 0;
         protected Transform TargetTransform;
 
-        private int _attackerId;
-        private float _damage;
+        public virtual void Spawn(int attackerId, Pose pose, BulletData bulletData, int focusTargetId,
+            Transform targetTransform = null)
+        {
+            _attackerId = attackerId;
+            _focusTargetId = focusTargetId;
+            transform.SetPositionAndRotation(pose.position, pose.rotation);
+            BulletData = bulletData;
+            Destroy(gameObject, 5.0f);
+        }
 
         protected virtual void Awake()
         {
-        }
-
-        protected virtual void Start()
-        {
             _hitCollider.OnTriggerEnterAsObservable()
-                .Subscribe(x =>
-                {
-                    if (x.gameObject.layer == _shooterLayer)
-                    {
-                        Debug.Log("BaseBulletView:Hit - Shooter Layer");
-                        return;
-                    }
-
-                    if (x.gameObject.CompareTag(TagConstants.Bullet))
-                    {
-                        Debug.Log("BaseBulletView:Hit - Bullet");
-
-                        return;
-                    }
-
-                    if (!x.TryGetComponent<IHitBoxView>(out var hitBox))
-                    {
-                        Debug.Log("BaseBulletView:Hit - No HitBox" + x.gameObject.name);
-                        Destroy(gameObject);
-                        return;
-                    }
-
-                    if (hitBox.Id == _attackerId)
-                    {
-                        Debug.Log("BaseBulletView:Hit - AttackerId");
-                        return;
-                    }
-
-                    hitBox.OnHit(_damage, _attackerId, transform.position, out _);
-                    Destroy(gameObject);
-                })
+                .Where(_ => CanHit)
+                .Subscribe(HitProcess)
                 .AddTo(this);
         }
 
@@ -63,14 +46,71 @@ namespace App.Battle.Views.Enemy.Bullet
         {
         }
 
-        public virtual void Spawn(int attackerId, Pose pose, BulletData bulletData, int focusTargetId,
-            Transform targetTransform = null)
+        private void HitProcess(Collider col)
         {
-            _attackerId = attackerId;
+            if (col.gameObject.layer == _shooterLayer)
+            {
+                return;
+            }
 
-            transform.SetPositionAndRotation(pose.position, pose.rotation);
-            _damage = bulletData.Damage;
-            Speed = bulletData.Speed;
+            if (col.gameObject.CompareTag(TagConstants.Bullet))
+            {
+                return;
+            }
+
+            if (!col.TryGetComponent<IHitBoxView>(out var hitBox))
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            if (hitBox.Id == _attackerId)
+            {
+                return;
+            }
+
+            if (_hitTargetIds.Contains(hitBox.Id))
+            {
+                return;
+            }
+
+            _hitTargetIds.Add(hitBox.Id);
+
+            hitBox.OnHit(BulletData.Damage, _attackerId, transform.position, out var canPenetrable);
+
+            if (_focusTargetId == hitBox.Id)
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            if (!canPenetrable)
+            {
+                HitAfterProcess().Forget();
+                return;
+            }
+
+            _hitCount++;
+
+            if (_focusTargetId != default && _hitCount >= BulletData.Penetration)
+            {
+                HitAfterProcess().Forget();
+            }
+        }
+
+        private async UniTask HitAfterProcess()
+        {
+            CanHit = false;
+
+            if (_trailRenderer == null)
+            {
+                return;
+            }
+
+            var time = _trailRenderer.time *= 0.5f;
+            _trailRenderer.time = time;
+            await UniTask.WaitForSeconds(time);
+            Destroy(gameObject);
         }
     }
 }
